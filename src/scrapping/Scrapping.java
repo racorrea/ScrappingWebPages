c20c6a4bc645a4205fe9d14553df2ecae667b1bb
roddycorrea <utpl@172.18.50.33>
2014-06-24 11:26:05 -0500

    Scrapping v1.1

--- src/scrapping/Scrapping.java
+++ src/scrapping/Scrapping.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package scrapping;
 
 import java.io.IOException;
@@ -11,34 +5,30 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 /**
  *
- * @author utpl
+ * @author roddycorrea
  */
 public class Scrapping {
 
-    /**
-     * @param args the command line arguments
-     * @throws java.io.IOException
-     */
     public static void main(String[] args){
-        String url = "http://ocw.mit.edu/courses/aeronautics-and-astronautics/16-06-principles-of-automatic-control-fall-2012/lecture-notes/";
+        String url = "http://ocw.mit.edu/courses/new-courses/";
+        int i=0;
         Document document;
         try {
             document = Jsoup.connect(url).get();
-            Elements stats = document.select("table tbody tr");
-            for(int i = 0; i < stats.size(); i++){
-                System.out.println(stats.get(i).text());
-            }
+            Elements stats = document.select("table.courseList tbody tr ");
+            Elements title = document.select("h3.deptTitle");
+            Elements link = document.select("a.preview[href]");
+                do{
+                        System.out.println("["+i+"] *** "+title.get(i).text() + "\n" + stats.get(i).text() +"\n\n");
+                    i++;
+                }while(i < stats.size());
         } catch (IOException ex) {
             Logger.getLogger(Scrapping.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-        
-
-        
     }
-    
 }
